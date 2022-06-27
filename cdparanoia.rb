8f7bb0f80e51df40cb95a90690b4572f36d1034c
Mark Norman Francis <norm@201created.com>
2022-06-27 11:16:56 +0100

    Update cdparanoia.rb

--- cdparanoia.rb
+++ cdparanoia.rb
@@ -1,20 +1,23 @@
 class Cdparanoia < Formula
   desc "Audio extraction tool for sampling CDs"
   homepage "https://www.xiph.org/paranoia/"
-  url "http://downloads.xiph.org/releases/cdparanoia/cdparanoia-III-10.2.src.tgz"
+  url "https://downloads.xiph.org/releases/cdparanoia/cdparanoia-III-10.2.src.tgz", using: :homebrew_curl
+  mirror "https://ftp.osuosl.org/pub/xiph/releases/cdparanoia/cdparanoia-III-10.2.src.tgz"
   sha256 "005db45ef4ee017f5c32ec124f913a0546e77014266c6a1c50df902a55fe64df"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+
+  livecheck do
+    url "https://ftp.osuosl.org/pub/xiph/releases/cdparanoia/?C=M&O=D"
+    regex(/href=.*?cdparanoia-III[._-]v?(\d+(?:\.\d+)+)\.src\.t/i)
+  end
 
   depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   # Patches via MacPorts
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/2a22152/cdparanoia/osx_interface.patch"
     sha256 "3eca8ff34d2617c460056f97457b5ac62db1983517525e5c73886a2dea9f06d9"
-  end
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/2a22152/cdparanoia/patch-paranoia_paranoia.c.10.4.diff"
-    sha256 "4d6d51909d1b29a3c8ac349f5132a8acd96628355117efb3f192408d2cc4829e"
   end
 
   # Personal patch to provide "silent output but still with progress bar"
@@ -24,7 +27,8 @@ class Cdparanoia < Formula
   end
 
   def install
-    system "autoconf"
+    system "autoreconf", "-fiv"
+   
     # Libs are installed as keg-only because most software that searches for cdparanoia
     # will fail to link against it cleanly due to our patches
     system "./configure", "--prefix=#{prefix}",
